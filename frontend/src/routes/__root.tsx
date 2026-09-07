986f61dee7116409b9effb1080ce7171ce282803
Jc <107809331+y3eet@users.noreply.github.com>
2026-09-07 16:42:25 +0800

    feat: add dashboard block

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,10 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: () => (
+    <TooltipProvider>
+      <Outlet />
+    </TooltipProvider>
+  ),
 });
--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -1,3 +1,9 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard")({
@@ -7,7 +13,20 @@ export const Route = createFileRoute("/dashboard")({
 function RouteComponent() {
   return (
     <div>
-      <Outlet />
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
     </div>
   );
 }
