3c1eb5fe50aa2f92eefeb57687934092a7014d6c
Jc <107809331+y3eet@users.noreply.github.com>
2026-09-08 09:51:18 +0800

    feat: add user crud

---
+++ app/crud/user.py
@@ -0,0 +1,32 @@
+from sqlmodel import Session, select
+
+from app.models.user import User, UserCreate, UserUpdate
+
+
+class UserCrud:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_user_by_email(self, email: str) -> User | None:
+        return self.session.exec(select(User).where(User.email == email)).first()
+
+    def get_user_by_id(self, user_id: int) -> User | None:
+        return self.session.exec(select(User).where(User.id == user_id)).first()
+
+    def create_user(self, user_create: UserCreate) -> User:
+        user = User.model_validate(user_create)
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        return user
+
+    def update_user(self, user: User, user_update: UserUpdate) -> User:
+        user_data = user.model_dump(exclude_unset=True)
+        update_data = user_update.model_dump(exclude_unset=True)
+        for field in user_data:
+            if field in update_data:
+                setattr(user, field, update_data[field])
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        return user
